0ff9ca9ce7928ec4a138449c39e2878b68a60271
Ashish Singh <Ashish.Singh@sscinc.com>
2024-08-18 16:23:01 +0530

    convert from standalone to module based component for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { TasksComponent } from './tasks/tasks.component';
+// import { HeaderComponent } from './header/header.component';
+// import { UserComponent } from './user/user.component';
+// import { TaskComponent } from './tasks/task/task.component';
+// import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent,TasksComponent],
+  // standalone: true,
+  // imports: [HeaderComponent, UserComponent, TaskComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TaskComponent } from "./tasks/task/task.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+ declarations:[AppComponent],
+ bootstrap: [ AppComponent],
+ imports: [BrowserModule, HeaderComponent, UserComponent, TaskComponent,TasksComponent]
+})
+
+export class AppModule{
+
+}
