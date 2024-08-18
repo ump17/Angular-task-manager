527961f78eadc214da0e06f0f31c8c164cd04324
Ashish Singh <Ashish.Singh@sscinc.com>
2024-08-18 17:04:31 +0530

    implemented shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { sharedModule } from "./shared/shared.module";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -7,14 +8,13 @@ import { UserComponent } from "./user/user.component";
 import { TaskComponent } from "./tasks/task/task.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { CardComponent } from "./shared/card/card.component";
 import { AddedTaskComponent } from "./tasks/added-task/added-task.component";
 
 
 @NgModule({
- declarations:[AppComponent, HeaderComponent, UserComponent, CardComponent, TaskComponent, TasksComponent, AddedTaskComponent],
+ declarations:[AppComponent, HeaderComponent, UserComponent, TaskComponent, TasksComponent, AddedTaskComponent],
  bootstrap: [ AppComponent],
- imports: [BrowserModule, FormsModule]
+ imports: [BrowserModule, FormsModule, sharedModule]
 })
 
 export class AppModule{
